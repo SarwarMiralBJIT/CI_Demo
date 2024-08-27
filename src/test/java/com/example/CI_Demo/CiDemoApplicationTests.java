af17b6cdfabb08ab47cdf7beb97143d2f9351489
Sarwar Miral <sarwar.miral@bjitgroup.com>
2024-08-27 18:10:10 +0600

    Added Tests

--- src/test/java/com/example/CI_Demo/CiDemoApplicationTests.java
+++ src/test/java/com/example/CI_Demo/CiDemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.CI_Demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CiDemoApplicationTests {
 
+	static Logger logger = LoggerFactory.getLogger(CiDemoApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("It's a test class");
+		assertEquals(true, true);
 	}
-
 }
